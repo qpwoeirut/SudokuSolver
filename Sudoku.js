ea949815ef5d86254e56fd07a578a9c7a7708341
qpwoeirut <stanleyzhong8@gmail.com>
2022-06-15 17:46:44 -0700

    Add grid

--- Sudoku.js
+++ Sudoku.js
@@ -13,28 +13,48 @@ function Sudoku() {
 }
 Sudoku.prototype.solve = () => {
     let possibilities = [];
+    let stack = [];
+    let used = [];
 
     for (let r = 0; r < SIZE; ++r) {
         possibilities.push([]);
+        used.push([]);
         for (let c = 0; c < SIZE; ++c) {
             possibilities[r].push(new Set());
-            for (let n = 1; n <= 9; ++n) {
-                possibilities[r][c].add(n);
-            }
-        }
-    }
-
-    let stack = [];
-    for (let r = 0; r < SIZE; ++r) {
-        for (let c = 0; c < SIZE; ++c) {
             if (this.grid[r][c] !== UNKNOWN) {
                 stack.push([r, c, this.grid[r][c]]);
+                possibilities[r][c].add(this.grid[r][c]);
+                used[r][c] = true;
+            } else {
+                for (let n = 1; n <= 9; ++n) {
+                    possibilities[r][c].add(n);
+                }
+                used[r][c] = false;
             }
         }
     }
 
     while (stack.length > 0) {
         let [r, c, val] = stack.pop();
-
+        for (let i = 0; i < SIZE; ++i) {
+            if (i !== r) {
+                possibilities[i][c].delete(val);
+                if (possibilities[i][c].size === 1 && used[i][c] === false) {
+                    used[i][c] = true;
+                    let [only_val] = possibilities[i][c];
+                    this.grid[i][c] = only_val;
+                    stack.push([i, c, only_val]);
+                }
+            }
+            if (i !== c) {
+                possibilities[r][i].delete(val);
+                if (possibilities[r][i].size === 1 && used[r][i] === false) {
+                    used[r][i] = true;
+                    let [only_val] = possibilities[r][i];
+                    this.grid[r][i] = only_val;
+                    stack.push([r, i, only_val]);
+                }
+            }
+        }
     }
 }
---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("sudokuContainer");
+for (let r = 0; r < SIZE; ++r) {
+    for (let c = 0; c < SIZE; ++c) {
+        const cell = document.createElement("div");
+        cell.classList.add("sudoku-cell");
+
+        if (r % 3 === 0) cell.classList.add("bold-top");
+        else if (r % 3 === 2) cell.classList.add("bold-bottom");
+
+        if (c % 3 === 0) cell.classList.add("bold-left");
+        else if (c % 3 === 2) cell.classList.add("bold-right");
+
+        const input = document.createElement("input");
+        input.setAttribute("data-row", r.toString());
+        input.setAttribute("data-col", c.toString());
+        input.type = "number";
+        input.min = "1";
+        input.max = "9";
+        input.oninput = () => {  // prevent multiple digits from being entered
+            while (input.value > SIZE) input.value = Math.floor(parseInt(input.value) / 10).toString()
+        }
+        cell.appendChild(input);
+        container.appendChild(cell);
+    }
+}
